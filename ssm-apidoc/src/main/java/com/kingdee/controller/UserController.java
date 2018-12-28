840f7ee33c593d9f66e8235edbf62df71e4548cb
kither <1406754132@qq.com>
2018-12-28 22:22:58 +0800

    Merge branch 'master' of https://github.com/KitherRunner/springproject
    # Conflicts:
    #	.idea/compiler.xml
    #	.idea/encodings.xml
    #	.idea/misc.xml
    #	.idea/modules.xml

--- ssm-apidoc/src/main/java/com/kingdee/controller/UserController.java
+++ ssm-apidoc/src/main/java/com/kingdee/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @MapperScan("com.kingdee.mapper")
@@ -19,8 +20,9 @@ public class UserController {
     private UserService userService;
 
 
-    @GetMapping(value = "user/{id}", produces = "application/json;chaeset=UTF-8")
+    @GetMapping(value = "/user/{id}", produces = "application/json;charset=utf-8")
     @ApiOperation(value = "find", notes = "根据id查询用户", httpMethod = "GET")
+    @ResponseBody
     public User find(@PathVariable("id") Integer id) {
         return userService.find(id);
     }
