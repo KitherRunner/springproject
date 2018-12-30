c16faa46c006524ec261ed3e8e6059fa2da07cad
kither <1406754132@qq.com>
2018-12-30 11:57:37 +0800

    提交动态生成sql语句的类

--- ssm-apidoc/src/main/java/com/kingdee/sqlProvider/MapperSqlProvider.java
+++ ssm-apidoc/src/main/java/com/kingdee/sqlProvider/MapperSqlProvider.java
@@ -8,8 +8,9 @@ public class MapperSqlProvider {
     public String addUser(User user) {
         return new SQL() {{
 
-            INSERT_INTO("user");
-            INTO_COLUMNS("name", "password", "gender");
+            INSERT_INTO("user"); // 插入数据的表
+            INTO_COLUMNS("name", "password", "gender"); // 插入数据的字段
+            // 对应的值(自己加引号)
             INTO_VALUES("\"" + user.getName() + "\"", "\"" + user.getPassword() + "\"", "\"" + user.getGender() + "\"");
         }}.toString();
     }
