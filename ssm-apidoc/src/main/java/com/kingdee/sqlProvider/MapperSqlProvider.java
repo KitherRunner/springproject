5e792d2016def95a6617f226dda6789ce5b689da
kither <1406754132@qq.com>
2019-01-02 21:06:21 +0800

    commit

--- ssm-apidoc/src/main/java/com/kingdee/sqlProvider/MapperSqlProvider.java
+++ ssm-apidoc/src/main/java/com/kingdee/sqlProvider/MapperSqlProvider.java
@@ -1,4 +1,4 @@
-package com.kingdee.sqlProvider;
+package com.kingdee.sqlprovider;
 
 import com.kingdee.bean.User;
 import org.apache.ibatis.jdbc.SQL;
@@ -7,9 +7,10 @@ public class MapperSqlProvider {
 
     public String addUser(User user) {
         return new SQL() {{
-
-            INSERT_INTO("user"); // 插入数据的表
-            INTO_COLUMNS("name", "password", "gender"); // 插入数据的字段
+            // 插入数据的表
+            INSERT_INTO("user");
+            // 插入数据的字段
+            INTO_COLUMNS("name", "password", "gender");
             // 对应的值(自己加引号)
             INTO_VALUES("\"" + user.getName() + "\"", "\"" + user.getPassword() + "\"", "\"" + user.getGender() + "\"");
         }}.toString();
