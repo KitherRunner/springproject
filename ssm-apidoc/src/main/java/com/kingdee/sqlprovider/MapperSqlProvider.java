5c3f894c59126ce4fdba7bb6bbeeadefb4798798
kither <1406754132@qq.com>
2019-01-02 13:26:59 +0800

    修改MapperSqlProvider

--- ssm-apidoc/src/main/java/com/kingdee/sqlprovider/MapperSqlProvider.java
+++ ssm-apidoc/src/main/java/com/kingdee/sqlprovider/MapperSqlProvider.java
@@ -1,23 +1,17 @@
 package com.kingdee.sqlprovider;
 
-import com.google.common.base.Strings;
 import com.kingdee.bean.User;
 import org.apache.ibatis.jdbc.SQL;
 
 public class MapperSqlProvider {
 
-    /**
-     * 动态构造添加用户信息的sql语句
-     *
-     * @param user
-     * @return
-     */
     public String addUser(User user) {
         return new SQL() {{
+            // 插入数据的表
             INSERT_INTO("user");
+            // 插入数据的字段
             INTO_COLUMNS("name", "password", "gender");
-
-            // 两头需要自己拼上字符串
+            // 对应的值(自己加引号)
             INTO_VALUES("\"" + user.getName() + "\"", "\"" + user.getPassword() + "\"", user.getGender().toString());
         }}.toString();
     }
