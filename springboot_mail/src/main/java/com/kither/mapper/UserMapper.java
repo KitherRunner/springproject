0d07ea3ad5cd63c1f0a9e2737d7018cf2bf57773
kither <1406754132@qq.com>
2019-04-08 18:29:06 +0800

    commit springboot_mail

--- springboot_mail/src/main/java/com/kither/mapper/UserMapper.java
+++ springboot_mail/src/main/java/com/kither/mapper/UserMapper.java
@@ -6,6 +6,6 @@ import org.apache.ibatis.annotations.Update;
 @Mapper
 public interface UserMapper {
 
-    @Update("update user set active = '1' where id = 1")
+    @Update("update user set active = '1' where id = #{id}")
     int avtiveUser(Integer id);
 }
