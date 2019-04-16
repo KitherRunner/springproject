8cf4ca015bc266c822e1508f18e061ebbfcb47a5
kither <1406754132@qq.com>
2019-04-16 23:23:52 +0800

    commit springmvc_rabbitmq

--- springmvc_rabbitmq/src/main/java/com/kither/provider/RabbitProvider.java
+++ springmvc_rabbitmq/src/main/java/com/kither/provider/RabbitProvider.java
@@ -15,10 +15,11 @@ public class RabbitProvider {
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
+    // 此处使用的是一对一的转换器，必须routingKey一直才能正确接收到消息
     public void sendMessage(Object object) {
         // 第一个参数为发送队列的名称，第二个参数为发送消息内容
         String json = new Gson().toJson(object);
-        rabbitTemplate.convertAndSend("first-queue", json);
+        rabbitTemplate.convertAndSend("sendDirect", json);
         System.out.println("消息提供者发送消息");
     }
 }
